454f745994686b6467efa49be9c68a0f3cee08bf
Nandisa <shangenandisa@gmail.com>
2025-11-10 13:30:09 +0200

    Added layout to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Layout from './Compenents/Layout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Layout/>
     </>
   )
 }
