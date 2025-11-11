96be4dff9faca9b918dff8d71ff13b06eb17c59c
Nandisa <shangenandisa@gmail.com>
2025-11-11 13:31:50 +0200

    added lucide library packages

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,51 +1,60 @@
 import React from "react";
+import { Home, Settings, User, BookOpen,NotepadText,CreditCard,Receipt,DollarSign,Repeat2,ChartNetwork,Calculator } from "lucide-react";
 
 export default function Layout() {
     return (
         <>
             <div className="sidebar-container">
                 <h1 className="main-heading">Finflow</h1>
-                <nav>
+                
+                <nav className="nav-links">
                     <div className="nav-container"> 
-                    <img src="public/book.png" width="35px"/>
+                    <Home size={24} />
                     <h2>Dashboard</h2>
                     </div>
                     <div className="nav-container">
-                    <img src="public/book.png" width="35px"/>
+                    <NotepadText/>
                     <h2>Entries</h2>
                     </div>
                     <div className="nav-container">
-                    <img src="public/book.png" width="35px"/>
+                    <BookOpen/>
                     <h2>Charts of Accounts</h2>
                     </div>
                     <div className="nav-container">
-                    <img src="public/book.png" width="35px"/>
+                    <CreditCard/>
                     <h2>Payments</h2>
                     </div>
                     <div className="nav-container">
-                    <img src="public/book.png" width="35px"/>
+                    <Receipt/>
                     <h2>Invoices</h2>
                     </div>
                     <div className="nav-container">
-                    <img src="public/book.png" width="35px"/>
+                    <DollarSign/>
                     <h2>Billings</h2>
                     </div>
                     <div className="nav-container">
+                   <Repeat2/>
                     <h2>Reconciliation</h2>
                     </div>
                     <div className="nav-container">
+                    <ChartNetwork/>
                     <h2>Reports</h2>
                     </div>
                     <div className="nav-container">
+                    <Calculator/>
                     <h2>VAT</h2>
                     </div>
                     <div className="nav-container">
+                    <Settings/>
                     <h2>Settings</h2>
                     </div>
                 </nav>
                 <footer>
+                    <span>
+                    <User/>
                     <p>Jane Doe</p>
                     <p>jane.doe@finflow.com</p>
+                    </span>
                 </footer>
             </div>
         </>
