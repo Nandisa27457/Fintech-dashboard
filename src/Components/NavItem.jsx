bea11aba5566920ca297d413a8f74fb31780ae28
Nandisa <shangenandisa@gmail.com>
2025-11-14 20:26:49 +0200

    Added a component for Nav items and styled using Material UI

--- src/Components/NavItem.jsx
+++ src/Components/NavItem.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Button, Box } from "@mui/material";
 
-
-export default function NavItem({icon,path,title}){
-    return(
-        <Link to= {path}>
-            {icon}
-            <h2>{title}</h2>
-        </Link>
-    )
+export default function NavItem({ icon, path, title }) {
+    return (
+        <Button
+            component={Link}
+            to={path}
+            sx={{
+                color: "black",
+                justifyContent: "flex-start",
+                textTransform: "none",
+                textDecoration: "none",
+                width: "100%",
+                padding: "10px 20px",
+                borderRadius: "12px",
+                "&:hover": {
+                    backgroundColor: "#996b3f5e",
+                    textDecoration: "none",
+                },
+            }}>
+            {title}
+        </Button>
+    );
 }
