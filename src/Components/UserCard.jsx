ed6e11384b9c31d6531fe83df8ec3fc088721b98
Nandisa <shangenandisa@gmail.com>
2025-11-14 20:27:07 +0200

    Created a User card component

---
+++ src/Components/UserCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function UserCard() {
+    return (
+        <>
+            <span>
+                <p className="Username">Jane Doe</p>
+            </span>
+            <p className="email">jane.doe@finflow.com</p>
+        </>
+    );
+}
